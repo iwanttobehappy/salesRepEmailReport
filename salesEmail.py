ebfbc075bff74b0756a99f1b51c1810e465c373b
Partha Mittra <partha.mittra@yahoo.com>
2013-01-09 18:32:08 -0500

    Initial Checkin
    This report requires the Doc to Practice and Doc to Sales Rep mappings
    which is local

---
+++ salesEmail.py
@@ -0,0 +1,160 @@
+import sys
+import csv
+import string
+from datetime import date
+import datetime
+import re
+
+def getDoctor(docName):
+	docName=re.sub("\d+",'',docName)
+	docName=docName.replace("^^^^^^^^^^NPI","")
+	tdocName=docName.rsplit('^')
+	docName=tdocName[2]+" "+tdocName[1]
+	return docName
+
+def getTestType(tt):
+	if tt[0]=='M':
+		return 'Molecular'
+	if tt[0]=='S':
+		return 'Surgical'
+	if tt[0]=='F':
+		if tt[1]=='H':
+			return 'FISH'
+		if tt[1]=='C':
+			return 'FLOW'
+	if tt[0]=='C':
+		return 'CYTO'
+
+		
+def getDirector(tt):
+	if tt=="Molecular":
+		return "Weiyi Chen"
+	if tt=="Surgical":
+		return "Lan Wang M.D."
+	if tt=="FISH":
+		return "Pal Kahlon Singh"
+	if tt=="CYTO":
+		return "Venkata Nandula"
+	if tt=="FLOW":
+		return "Kelly Corcoran"
+	
+
+def getTest(test):
+	return test
+
+def computeTAT(received,reported):
+	dateRec=makeDateObj(received)
+	dateRep=makeDateObj(reported)
+	return abs((dateRep-dateRec).days)
+	
+
+def makeDateObj(datestr):
+	year=datestr[0:4]
+	month=datestr[4:6]
+	day=datestr[6:8]
+	year=int(year)
+	month=int(month)
+	day=int(day)
+	return datetime.date(year,month,day)
+
+	
+def splitDocName(dn):
+	tmp=dn.split(";")
+	dn=tmp[1]+tmp[0]
+	return str.lstrip(dn)
+	
+def getPracFromDoc(doc):
+	global docToPrac
+	for k,v in docToPrac.items():
+		if k==doc:
+			return v
+	return "No Practice found for "+doc
+
+def getSalesRep(doc):
+	global docToSalesRep
+	for k,v in docToSalesRep.items():
+		if k==doc:
+			return v
+	return "No Sales Rep found for "+doc
+	
+
+if len(sys.argv)== 4:
+	hl7file=sys.argv[1]
+	docToPractice=sys.argv[2]
+	docToSales=sys.argv[3]
+
+	
+else:
+	print "USAGE: salesEmail.py <hl7 file> <docToPractice_csv> <docToSales.csv>"
+	print "bye bye"
+	sys.exit()
+
+	
+	
+# maps the doctors to practices	
+rr=csv.reader(open(docToPractice,'rb'),delimiter=',',quotechar='\'')
+rr.next()
+
+docToPrac=dict()
+for r in rr:
+	docToPrac[splitDocName(r[0])]=r[1]
+	
+#maps the doctors to sales reps
+rs=csv.reader(open(docToSales,'rU'),delimiter=',',quotechar='\'')
+rs.next()
+
+docToSalesRep=dict()
+for r in rs:
+	docToSalesRep[splitDocName(r[0])]=r[1]
+	
+	
+	
+	
+	
+	
+
+	
+	
+reportReader=csv.reader(open(hl7file,'rU'),delimiter='|',quotechar='\'')
+OrderingSite=""
+TestType=""
+TAT=0
+Test=""
+Physician=""
+Practice=""
+SalesRep=""
+SignOffDirector=""
+for row in reportReader:
+	if row[0]=='MSH' and TestType != "":
+		print "Test Type: "+TestType
+		print "Test: "+Test
+		print "TAT :"+str(TAT)
+		print "Doctor :"+Physician
+		print "Practice :"+getPracFromDoc(Physician)
+		print "Sales Rep :"+getSalesRep(Physician)
+		print "Signed Off By :"+SignOffDirector
+		print
+	if row[0]=='PV1':
+		Physician=getDoctor(row[7])
+	if row[0]=='PID':
+		TestType=getTestType(row[3])
+		SignOffDirector=getDirector(TestType)
+	if row[0]=='FT1' and not row[7].isdigit():
+		Test=getTest(row[7])
+		TAT=computeTAT(row[4],row[5])
+
+
+
+
+	
+		
+		
+
+
+	
+	
+	
+	
+	
+	
+
